1cd0561198ba069d116809c88a34289cbebc2067
Robert Greinacher <robert@3antworten.de>
2016-04-11 13:02:53 +0200

    Bootstrap4 & first sidebar work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'application#welcome'
+  root 'application#styleguide'
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
+
+  get '/styleguide' => 'application#styleguide'
 end
