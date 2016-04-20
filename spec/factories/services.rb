29a90d669fafa83dc073d1fcc8ea0f7d4ccf3a17
Robert Greinacher <robert@3antworten.de>
2016-04-20 18:21:41 +0200

    WIP: create tests for services

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -6,23 +6,21 @@ FactoryGirl.define do
     url 'http://localhost:3000'
     title 'Active Learning component'
     version 'v2.0.0-rc1'
-  end
 
-  factory :service_bootstrap do
-    roll 2
-    description 'Rorik algorithm for optimal results'
-    capability 1
-    url 'http://localhost:3000'
-    title 'NER Bootstrapper'
-    version 'v1.0.0.beta3'
-  end
+    factory :service_bootstrap do
+      roll 2
+      description 'Rorik algorithm for optimal results'
+      url 'http://localhost:3000'
+      title 'NER Bootstrapper'
+      version 'v1.0.0.beta3'
+    end
 
-  factory :service_machine_learning do
-    roll 3
-    description 'Arnnes algorithm for optimal results'
-    capability 1
-    url 'http://localhost:3000'
-    title 'NER algorithm'
-    version '1.8.3'
+    factory :service_machine_learning do
+      roll 3
+      description 'Arnnes algorithm for optimal results'
+      url 'http://localhost:3000'
+      title 'NER algorithm'
+      version '1.8.3'
+    end
   end
 end
