de64b97c497a72dd9104d35ac72aa5b50aac2684
Arik Grahl <mail@arik-grahl.de>
2016-04-26 10:42:58 +0200

    disable swagger documentation

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,24 +1,20 @@
 module API
   module V1
-    class BaseController < ApplicationController
-      include Swagger::Docs::ImpotentMethods
+    class BaseController < ActionController::Base
+      # swagger_controller :base, 'Base'
 
-      skip_before_filter :authenticate_user!
-      swagger_controller :base, 'Base'
+      # swagger_api :who_are_you do
+      # end
 
-      swagger_api :who_are_you do
-        response :ok, :Service
-      end
-
-      swagger_model :Service do
-        description 'A Service object.'
-        property_list :role, :string, :required, 'Role', [:active_learning, :bootstrap, :machine_learning]
-        property :title, :string, :required, 'Title'
-        property :description, :string, :optional, 'Description'
-        property_list :problem_id, :string, :required, 'Capability', [:ner]
-        property :url, :string, :required, 'URL'
-        property :version, :string, :required
-      end
+      # swagger_model :Service do
+      #   description 'A Service object.'
+      #   property_list :role, :string, :required, 'Role', [:active_learning, :bootstrap, :machine_learning]
+      #   property :title, :string, :required, 'Title'
+      #   property :description, :string, :optional, 'Description'
+      #   property_list :problem_id, :string, :required, 'Capability', [:ner]
+      #   property :url, :string, :required, 'URL'
+      #   property :version, :string, :required
+      # end
 
       def who_are_you
         render json: service_hash
