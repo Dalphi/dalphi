5d69461f70ae1d0814880d377c28f12d616d70b3
Arik Grahl <mail@arik-grahl.de>
2016-05-05 18:17:05 +0200

    initially working swagger documentation

--- app/controllers/api/v1/who_are_you_controller.rb
+++ app/controllers/api/v1/who_are_you_controller.rb
@@ -6,11 +6,18 @@ module API
       swagger_controller :who_are_you, 'WhoAreYou'
 
       swagger_api :who_are_you do
-        summary "Fetches all User items"
-        notes "This lists all the active users"
-        response :unauthorized
-        response :not_acceptable, "The request you made is not acceptable"
-        response :requested_range_not_satisfiable
+        summary 'Identifies the service that is operating at this URL'
+        response :ok, 'Success', :Service
+      end
+
+      swagger_model :Tag do
+        description 'A Service object.'
+        property_list :role, :string, :required, 'Role', ['active_learning', 'bootstrap', 'machine_learning']
+        property :title, :string, :required, 'Title'
+        property :description, :string, :required, 'Description'
+        property_list :problem_id, :string, :required, 'Proplem identifier', ['ner']
+        property :url, :string, :required, 'URL'
+        property :version, :string, :required, 'Version'
       end
 
       def who_are_you
