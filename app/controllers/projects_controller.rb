b2a0eaf0764aae5b864913fec29393635c02b65b
Robert Greinacher <robert@3antworten.de>
2016-05-17 15:51:20 +0200

    update project with services as a whole

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -50,32 +50,32 @@ class ProjectsController < ApplicationController
 
   # PATCH/PUT /projects/1
   def update
-    if @project.update(project_params)
-      redirect_to projects_path, notice: I18n.t('projects.action.update.success')
+    if @project.update(params_with_service_instances)
+      redirect_to project_path(@project), notice: I18n.t('projects.action.update.success')
     else
       flash[:error] = I18n.t('simple_form.error_notification.default_message')
       render :edit
     end
   end
 
-  # PATCH /projects/1/update_services
-  def update_service
-    service_id = params[:service]
-    service_symbol = service_id.to_sym
-    service_name = service_id[0..-9].tr('_', ' ').capitalize
-    new_service = Service.find(project_params[service_symbol])
-
-    if @project.update({ service_symbol => new_service })
-      translation_key = "projects.action.update-service.success"
-      redirect_to project_path(@project),
-        notice: I18n.t(translation_key, service: service_name)
-    else
-      render_edit_with_error 'error', service_name
-    end
-
-  rescue ActiveRecord::RecordNotFound
-    render_edit_with_error 'not-recognized', service_name
-  end
+  # # PATCH /projects/1/update_services
+  # def update_service
+  #   service_id = params[:service]
+  #   service_symbol = service_id.to_sym
+  #   service_name = service_id[0..-9].tr('_', ' ').capitalize
+  #   new_service = Service.find(project_params[service_symbol])
+  #
+  #   if @project.update({ service_symbol => new_service })
+  #     translation_key = "projects.action.update-service.success"
+  #     redirect_to project_path(@project),
+  #       notice: I18n.t(translation_key, service: service_name)
+  #   else
+  #     render_edit_with_error 'error', service_name
+  #   end
+  #
+  # rescue ActiveRecord::RecordNotFound
+  #   render_edit_with_error 'not-recognized', service_name
+  # end
 
   # DELETE /projects/1
   def destroy
@@ -97,6 +97,17 @@ class ProjectsController < ApplicationController
       }
     end
 
+    def params_with_service_instances
+      new_params = project_params
+
+      [:active_learning_service, :bootstrap_service, :machine_learning_service].each do |service_symbol|
+        service_instance = Service.find_by_id(project_params[service_symbol])
+        new_params[service_symbol] = service_instance
+      end
+
+      new_params
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
       params.require(:project).permit(
@@ -109,10 +120,10 @@ class ProjectsController < ApplicationController
       )
     end
 
-    # render edit and show error flash if setting a different service went wrong
-    def render_edit_with_error(error_key, service_name)
-      translation_key = "projects.action.update-service.#{error_key}"
-      flash[:error] = I18n.t(translation_key, service: service_name)
-      render :edit
-    end
+    # # render edit and show error flash if setting a different service went wrong
+    # def render_edit_with_error(error_key, service_name)
+    #   translation_key = "projects.action.update-service.#{error_key}"
+    #   flash[:error] = I18n.t(translation_key, service: service_name)
+    #   render :edit
+    # end
 end
