088acc2acd9a4bc4f8a794ef6db90f9b4808276d
Robert Greinacher <robert@3antworten.de>
2016-05-18 16:04:41 +0200

    included conn. check in all views displaying services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -39,11 +39,7 @@ class ServicesController < ApplicationController
 
   # GET /services/1/check_connectivity
   def check_connectivity
-    # render json: { serviceIsAvailable: @service.is_available }, status: 200
-
-    # use this to simulate connectivities:
-    sleep((500 + rand(3000)) / 1000.0)
-    render json: { serviceIsAvailable: [true, false].sample }, status: [200, 500].sample
+    render json: { serviceIsAvailable: @service.is_available }, status: 200
   end
 
   # POST /services
