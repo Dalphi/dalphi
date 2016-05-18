bf99f1c6087a4d02867e948f4d947243fa696f01
Arik Grahl <mail@arik-grahl.de>
2016-05-18 13:26:08 +0200

    implement multi file upload and zip extraction for file uploads

--- app/models/raw_datum.rb
+++ app/models/raw_datum.rb
@@ -1,8 +1,19 @@
 class RawDatum < ApplicationRecord
-  SHAPES = %w(text)
+  MIME_TYPES = {
+    text: [
+      'text/plain',
+      'text/markdown',
+      'text/html',
+      'text/rtf'
+    ],
+  }
+  SHAPES = MIME_TYPES.keys.map(&:to_s)
 
   belongs_to :project
   has_attached_file :data
+
+  validates :project,
+    presence: true
 
   validates :shape,
     presence: true,
@@ -13,43 +24,71 @@ class RawDatum < ApplicationRecord
 
   validates_attachment :data,
     content_type: {
-      content_type: [
-        'text/plain',
-        'text/markdown',
-        'text/html',
-        'text/rtf',
-        'application/zip'
-      ]
+      content_type: MIME_TYPES.values.first
     }
 
-  def self.zip_to_data(zip)
+  def self.batch_process(project, data)
+    data_size = data.size
+    data_first = data.first
+    if data_size == 1 && data_first.content_type =~ /\Aapplication/
+      batch_result = RawDatum.zip_to_data project, data_first.tempfile.path
+    elsif data_size >= 1
+      batch_data = []
+      data.each do |datum|
+        batch_data << { filename: datum.original_filename, path: datum.path }
+      end
+      batch_result = RawDatum.batch_create project, batch_data
+    end
+    batch_result
+  end
+
+  def self.zip_to_data(project, zip)
     temp_dir = Dir.mktmpdir
+    batch_result = { success: [], error: [] }
     begin
-      batch_result = { success: [], error: [] }
-      batch_result = process_zip_archive zip, temp_dir, batch_result
+      batch_result = process_zip_archive project, zip, temp_dir
     ensure
       FileUtils.remove_entry temp_dir
     end
-    return batch_result
+    batch_result
   end
 
-
-  def self.process_zip_archive(zip, temp_dir, batch_result)
+  def self.process_zip_archive(project, zip, temp_dir)
     require 'zip'
+    batch_result = { success: [], error: [] }
     Zip::File.open(zip) do |zipfile|
       zipfile.each do |file|
-        file_name = file.name
-        extracted_file = "#{temp_dir}/#{file_name}"
-        zipfile.extract(file, extracted_file)
+        filename = file.name
+        zipfile.extract(file, "#{temp_dir}/#{filename}")
         raw_datum = RawDatum.new(
+          project: project,
           shape: SHAPES.first,
-          data: File.open(extracted_file)
+          data: File.open("#{temp_dir}/#{filename}")
         )
         if raw_datum.save
-          batch_result[:success] << file_name
+          batch_result[:success] << filename
         else
-          batch_result[:error] << file_name
+          batch_result[:error] << filename
         end
+      end
+    end
+    batch_result
+  end
+
+  def self.batch_create(project, data)
+    batch_result = { success: [], error: [] }
+    data.each do |datum|
+      filename = datum[:filename]
+      raw_datum = RawDatum.new(
+        project: project,
+        shape: SHAPES.first,
+        data: File.open(datum[:path])
+      )
+      raw_datum.data_file_name = filename
+      if raw_datum.save
+        batch_result[:success] << filename
+      else
+        batch_result[:error] << filename
       end
     end
     batch_result
--- spec/models/raw_datum_spec.rb
+++ spec/models/raw_datum_spec.rb
@@ -35,10 +35,13 @@ RSpec.describe RawDatum, :type => :model do
       @raw_datum.shape = 'M0ST unl1kely sh4pe n4me'
       expect(@raw_datum).to be_invalid
     end
+  end
 
+  describe 'zip_to_data' do
     it 'can batch process a zip archive with valid files' do
       expect(RawDatum.all.count).to eq(0)
-      batch_result = RawDatum::zip_to_data("#{Rails.root}/spec/fixtures/zip/valid.zip")
+      batch_result = RawDatum.zip_to_data @raw_datum.project,
+                                           "#{Rails.root}/spec/fixtures/zip/valid.zip"
       expect(batch_result).to eq(
         {
           success: ['valid1.md', 'valid2.md'],
@@ -50,7 +53,8 @@ RSpec.describe RawDatum, :type => :model do
 
     it 'can batch process a zip archive with partially valid files' do
       expect(RawDatum.all.count).to eq(0)
-      batch_result = RawDatum::zip_to_data("#{Rails.root}/spec/fixtures/zip/partially-valid.zip")
+      batch_result = RawDatum.zip_to_data @raw_datum.project,
+                                           "#{Rails.root}/spec/fixtures/zip/partially-valid.zip"
       expect(batch_result).to eq(
         {
           success: ['valid.md'],
@@ -60,9 +64,63 @@ RSpec.describe RawDatum, :type => :model do
       expect(RawDatum.all.count).to eq(1)
     end
 
-    it 'can batch process a zip archive with partially valid files' do
+    it 'can batch process a zip archive with invalid files' do
       expect(RawDatum.all.count).to eq(0)
-      batch_result = RawDatum::zip_to_data("#{Rails.root}/spec/fixtures/zip/invalid.zip")
+      batch_result = RawDatum.zip_to_data @raw_datum.project,
+                                           "#{Rails.root}/spec/fixtures/zip/invalid.zip"
+      expect(batch_result).to eq(
+        {
+          success: [],
+          error: ['invalid1.bin', 'invalid2.bin']
+        }
+      )
+      expect(RawDatum.all.count).to eq(0)
+    end
+  end
+
+  describe 'batch_create' do
+    it 'can batch process a set of valid files' do
+      expect(RawDatum.all.count).to eq(0)
+      data = [
+        { filename: 'valid1.md', path: "#{Rails.root}/spec/fixtures/text/valid1.md" },
+        { filename: 'valid2.md', path: "#{Rails.root}/spec/fixtures/text/valid2.md" }
+      ]
+      batch_result = RawDatum.batch_create @raw_datum.project,
+                                           data
+      expect(batch_result).to eq(
+        {
+          success: ['valid1.md', 'valid2.md'],
+          error: []
+        }
+      )
+      expect(RawDatum.all.count).to eq(2)
+    end
+
+    it 'can batch process a set of partially valid files' do
+      expect(RawDatum.all.count).to eq(0)
+      data = [
+        { filename: 'valid.md', path: "#{Rails.root}/spec/fixtures/text/valid.md" },
+        { filename: 'invalid.bin', path: "#{Rails.root}/spec/fixtures/text/invalid.bin" }
+      ]
+      batch_result = RawDatum.batch_create @raw_datum.project,
+                                           data
+      expect(batch_result).to eq(
+        {
+          success: ['valid.md'],
+          error: ['invalid.bin']
+        }
+      )
+      expect(RawDatum.all.count).to eq(1)
+    end
+
+    it 'can batch process a set of invalid files' do
+      expect(RawDatum.all.count).to eq(0)
+      data = [
+        { filename: 'invalid1.bin', path: "#{Rails.root}/spec/fixtures/text/invalid1.bin" },
+        { filename: 'invalid2.bin', path: "#{Rails.root}/spec/fixtures/text/invalid2.bin" },
+      ]
+      batch_result = RawDatum.batch_create @raw_datum.project,
+                                           data
       expect(batch_result).to eq(
         {
           success: [],
@@ -110,5 +168,12 @@ RSpec.describe RawDatum, :type => :model do
     end
   end
 
-  it { should belong_to(:project) }
+  describe 'project' do
+    it 'should not be project-less' do
+      @raw_datum.project = nil
+      expect(@raw_datum).to be_invalid
+    end
+
+    it { should belong_to(:project) }
+  end
 end
