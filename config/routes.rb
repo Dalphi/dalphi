8fe52f367cd02fa8430f7d220b9d5679afc08d5a
Robert Greinacher <robert@3antworten.de>
2016-05-18 15:57:32 +0200

    multiline comment for 100chars per line rule

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,8 @@ Rails.application.routes.draw do
     resources :raw_data, except: [:show]
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
