b39aee35b0b809393b734655d4810573ed50adea
Robert Greinacher <robert@3antworten.de>
2016-05-25 14:51:35 +0200

    no lines with more than 100 chars

--- app/controllers/api/v1/who_are_you_controller.rb
+++ app/controllers/api/v1/who_are_you_controller.rb
@@ -12,12 +12,32 @@ module API
 
       swagger_model :Tag do
         description 'A Service object.'
-        property_list :role, :string, :required, 'Role', ['active_learning', 'bootstrap', 'machine_learning']
-        property :title, :string, :required, 'Title'
-        property :description, :string, :required, 'Description'
-        property_list :problem_id, :string, :required, 'Proplem identifier', ['ner']
-        property :url, :string, :required, 'URL'
-        property :version, :string, :required, 'Version'
+        property_list :role,
+                      :string,
+                      :required,
+                      'Role',
+                      ['active_learning', 'bootstrap', 'machine_learning']
+        property :title,
+                 :string,
+                 :required,
+                 'Title'
+        property :description,
+                 :string,
+                 :required,
+                 'Description'
+        property_list :problem_id,
+                 :string,
+                 :required,
+                 'Proplem identifier',
+                 ['ner']
+        property :url,
+                 :string,
+                 :required,
+                 'URL'
+        property :version,
+                 :string,
+                 :required,
+                 'Version'
       end
 
       def who_are_you
@@ -30,7 +50,8 @@ module API
         {
           role: 'webapp',
           title: 'Dalphi',
-          description: 'The Ruby on Rails Dalphi webapp for user interaction and service intercommunication',
+          description: 'The Ruby on Rails Dalphi webapp for user interaction \
+                       and service intercommunication',
           problem_id: 'ner',
           url: root_url,
           version: '1.0'
