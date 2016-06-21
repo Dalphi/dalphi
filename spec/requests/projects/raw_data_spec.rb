22bb6ad6f5ce6847e8faf68ff0e5ceaa5c218a38
Arik Grahl <mail@arik-grahl.de>
2016-06-21 16:21:08 +0200

    implement request test for xhr raw_data

--- spec/requests/projects/raw_data_spec.rb
+++ spec/requests/projects/raw_data_spec.rb
@@ -2,21 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "RawData API", type: :request do
   it 'shows raw data' do
-    sign_in
     raw_datum = FactoryGirl.create(:raw_datum)
-    get "/projects/#{raw_datum.project.id}/raw_data", format: :json
-    ap response.body
+    sign_in(raw_datum.project.user)
+
+    xhr :get, "/projects/#{raw_datum.project.id}/raw_data"
     expect(response).to be_success
+
     json = JSON.parse(response.body)
     expect(json).to eq(
-      {
-        'id' => 1,
-        'interface_type' => 'text_nominal',
-        'payload' => "{\"label\":\"testlabel\",\"options\":[\"option1\",\"option2\"],\"content\":\"testcontent\"}",
-        'rank' => nil,
-        'raw_datum_id' => 1,
-        'skipped' => nil
-      }
+      [
+        {
+         'id' => 1,
+         'shape' => 'text',
+         'created_at' => json_datetime(raw_datum.created_at),
+         'updated_at' => json_datetime(raw_datum.updated_at),
+         'data_file_name' => 'lorem.txt',
+         'data_content_type' => 'text/plain',
+         'data_file_size' => 1302,
+         'data_updated_at' => json_datetime(raw_datum.data_updated_at),
+         'project_id' => 1,
+         'filename' => 'file.md'
+        }
+      ]
     )
   end
 end
