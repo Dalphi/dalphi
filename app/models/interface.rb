c424477df83a7a72175e5d6091b13670333684a3
Robert Greinacher <robert@3antworten.de>
2016-06-29 13:39:52 +0200

    writing validators for (s)css and JS uploads

--- app/models/interface.rb
+++ app/models/interface.rb
@@ -1,3 +1,16 @@
 class Interface < ApplicationRecord
   serialize :associated_problem_identifiers, Array
+
+  validates :interface_type, :associated_problem_identifiers,
+    presence: true
+
+  validates :title, :template,
+    presence: true,
+    uniqueness: true
+
+  validate do |interface|
+    WebsiteComponentsValidator.validate_stylesheet(interface)
+    WebsiteComponentsValidator.validate_java_script(interface)
+  end
+
 end
