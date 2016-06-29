3041896556b063f97e30b12689320f26402a9bcf
Robert Greinacher <robert@3antworten.de>
2016-06-29 14:45:14 +0200

    scss & coffee script validation

--- app/validators/website_components_validator.rb
+++ app/validators/website_components_validator.rb
@@ -9,11 +9,16 @@ class WebsiteComponentsValidator < ActiveModel::Validator
     sass_engine = Sass::Engine.new(record.stylesheet, syntax: :scss)
     sass_engine.render
   rescue Sass::SyntaxError
-    error_message = I18n.t('activerecord.errors.models.service.attributes.url.badHttpResponse')
-    record.errors[url] << error_message
+    error_message = I18n.t('activerecord.errors.models.interface.attributes.stylesheet.syntax-error')
+    record.errors['stylesheet'] << error_message
   end
 
   def self.validate_java_script(record)
     return if record.java_script.nil?
+
+    CoffeeScript.compile record.java_script
+  rescue ExecJS::RuntimeError
+    error_message = I18n.t('activerecord.errors.models.interface.attributes.java_script.syntax-error')
+    record.errors['java_script'] << error_message
   end
 end
