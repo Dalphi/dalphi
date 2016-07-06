c2fde233a01f30f3f1ffcc00f0861dc7f259e8d9
Robert Greinacher <robert@3antworten.de>
2016-07-06 18:24:16 +0200

    removed wrong written db table col

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,13 @@ ActiveRecord::Schema.define(version: 20160706141042) do
 
   create_table "annotation_documents", force: :cascade do |t|
     t.string   "interface_type"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.integer  "raw_datum_id"
     t.integer  "project_id"
     t.text     "payload"
     t.integer  "rank"
     t.boolean  "skipped"
-    t.boolean  "displayed",      default: false
     t.index ["project_id"], name: "index_annotation_documents_on_project_id"
     t.index ["raw_datum_id"], name: "index_annotation_documents_on_raw_datum_id"
   end
@@ -50,7 +49,6 @@ ActiveRecord::Schema.define(version: 20160706141042) do
     t.text     "java_script"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.text     "compiled_javascript"
     t.text     "compiled_stylesheet"
     t.text     "compiled_java_script"
   end
@@ -92,9 +90,8 @@ ActiveRecord::Schema.define(version: 20160706141042) do
     t.string   "url"
     t.string   "title"
     t.string   "version"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.text     "interface_types"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
