1583139fc757546ef36fcdbd53d40ddd06ecd95b
Arik Grahl <mail@arik-grahl.de>
2016-08-25 19:53:48 +0200

    satisfy linting

--- db/migrate/20160825174312_refactor_bootstrap_service_to_iterate_service.rb
+++ db/migrate/20160825174312_refactor_bootstrap_service_to_iterate_service.rb
@@ -1,6 +1,8 @@
 class RefactorBootstrapServiceToIterateService < ActiveRecord::Migration[5.0]
   def change
     rename_column :projects, :bootstrap_service_id, :iterate_service_id
-    rename_index :projects, 'index_projects_on_bootstrap_service_id', 'index_projects_on_iterate_service_id'
+    rename_index :projects,
+                 'index_projects_on_bootstrap_service_id',
+                 'index_projects_on_iterate_service_id'
   end
 end
