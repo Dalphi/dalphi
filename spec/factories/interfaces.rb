aef9c92ad174544e4f6e321ede908c1fe693ec64
Robert Greinacher <rg@rgPro.fritz.box>
2016-09-07 20:22:25 +0200

    removed file references from factories

--- spec/factories/interfaces.rb
+++ spec/factories/interfaces.rb
@@ -3,9 +3,6 @@ FactoryGirl.define do
     title 'Fast NER Annotion'
     interface_type 'text_nominal'
     associated_problem_identifiers ['ner']
-    #java_script File.new("#{Rails.root}/spec/fixtures/interfaces/text_nominal.js")
-    #stylesheet File.new("#{Rails.root}/spec/fixtures/interfaces/text_nominal.css")
-    #template File.new("#{Rails.root}/spec/fixtures/interfaces/text_nominal.html")
 
     factory :interface_2 do
       title 'another NER annotator'
