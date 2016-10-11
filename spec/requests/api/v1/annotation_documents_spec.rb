d1af99b3be870527d80eecc6a68a7bead99dfc3e
Robert Greinacher <rg@rgPro.fritz.box>
2016-10-11 20:39:33 +0200

    save annotations correctly; closes #216

--- spec/requests/api/v1/annotation_documents_spec.rb
+++ spec/requests/api/v1/annotation_documents_spec.rb
@@ -82,6 +82,40 @@ RSpec.describe "AnnotationDocuments API", type: :request do
     expect(annotation_document.skipped).to eq(true)
   end
 
+  it 'patches a JSON stringify encoded annotation document' do
+    annotation_document = FactoryGirl.create(:annotation_document)
+    expect(AnnotationDocument.all.count).to eq(1)
+
+    file_path = Rails.root.join('spec/fixtures/text/annotation_documnent_payload_real_world.txt')
+    annotation_document_definition = IO.read(file_path)
+
+    patch "/api/v1/annotation_documents/#{annotation_document.id}",
+          annotation_document_definition,
+          {
+            'CONTENT_TYPE' => 'application/json',
+            'ACCEPT' => 'application/json'
+          }
+
+    expect(response).to be_success
+    expect(AnnotationDocument.all.count).to eq(1)
+
+    json = JSON.parse(response.body)
+    expect(json['interface_type']).to eq('ner_complete')
+    expect(json['raw_datum_id']).to eq(1)
+    expect(json['rank']).to eq(27)
+    expect(json['skipped']).to eq(false)
+    expect(json['payload']['content'][0][0][0]['term']).to eq('Anleihe')
+
+    annotation_document.reload
+    expect(annotation_document.interface_type.name).to eq('ner_complete')
+    expect(annotation_document.raw_datum_id).to eq(1)
+    expect(annotation_document.rank).to eq(27)
+    expect(annotation_document.skipped).to eq(false)
+
+    parsed_payload = annotation_document.relevant_attributes[:payload]
+    expect(parsed_payload['content'][0][0][0]['term']).to eq('Anleihe')
+  end
+
   it 'destroys an annotation document' do
     annotation_document = FactoryGirl.create(:annotation_document)
     expect(AnnotationDocument.all.count).to eq(1)
