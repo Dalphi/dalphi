94d2996457e48b6c3bd1a87f05d4233de878e43b
Arik Grahl <mail@arik-grahl.de>
2016-10-11 15:51:35 +0200

    basic presence test

--- spec/models/annotator_spec.rb
+++ spec/models/annotator_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Annotator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @annotator = FactoryGirl.create(:annotator)
+  end
+
+  it 'should have a valid factory' do
+    expect(@annotator).to be_valid
+  end
+
+  it { should validate_presence_of(:name) }
 end
