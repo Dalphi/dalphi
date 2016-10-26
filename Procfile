a9d740654cecfad7d4012dd1e5dccfc81740582e
Arik Grahl <mail@arik-grahl.de>
2016-10-26 14:57:25 +0200

    implement mailing for assigning annotators to project

--- Procfile
+++ Procfile
@@ -1,5 +1,2 @@
 web: rails s puma -p 3000 -b 0.0.0.0
 worker: rails jobs:work
-iterate: rails dummy_service:iterate
-merge: rails dummy_service:merge
-machine_learning: rails dummy_service:machine_learning
--- app/models/annotator.rb
+++ app/models/annotator.rb
@@ -6,10 +6,20 @@ class Annotator < ApplicationRecord
 
   has_many :annotation_documents, as: :annotable
 
-  has_and_belongs_to_many :projects
+  has_and_belongs_to_many :projects,
+                          after_add: :assign_to_project,
+                          after_remove: :unassign_from_project
 
   validates :name,
             presence: true
+
+  def assign_to_project(project)
+    AnnotatorProjectAssignmentMailer.assign(self, project).deliver_later
+  end
+
+  def unassign_from_project(project)
+    AnnotatorProjectAssignmentMailer.unassign(self, project).deliver_later
+  end
 
   def label
     name
