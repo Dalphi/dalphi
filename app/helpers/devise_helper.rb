c673d5bda6819b147ad8ef2def1e3642acaa0b50
Arik Grahl <mail@arik-grahl.de>
2016-11-02 16:36:48 +0100

    remove legacy file `app/helpers/devise_helper.rb`

--- app/helpers/devise_helper.rb
+++
@@ -1,6 +0,0 @@
-module DeviseHelper
-  def devise_error_messages!
-    return '' if user_signed_in? || resource.errors.empty?
-    flash.now[:error] = I18n.t('simple_form.error_notification.default_message')
-  end
-end
